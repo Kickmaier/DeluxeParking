7b5f7de0454e5aa98ec523de6c93f605e100da24
Tobias Kickmaier <t.kickmaier@hotmail.com>
2025-11-11 17:37:21 +0100

    Skapat klasser

--- Vehicle.cs
+++ Vehicle.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DeluxeParking
 {
-    internal class Vehicle
+    abstract class Vehicle
     {
+        public string LicensePlate { get; set; }
+        public string VehicleColor { get; set; }
+        public abstract float VehicleSize { get; }
+
+        protected Vehicle(string licensePlate, string vehicleColor)
+        {
+            LicensePlate = licensePlate;
+            VehicleColor = vehicleColor;
+        }
+    }
+
+    internal class Motorcycle : Vehicle
+    {
+        public Motorcycle(string licensePlate, string vehicleColor) : base(licensePlate, vehicleColor) { }
+        public string Brand { get; set; }
+        public override float VehicleSize { get; } = 0.5f;
+    }
+    internal class Car : Vehicle
+    {
+        public Car (string licensePlate, string vehicleColor) : base (licensePlate, vehicleColor) { }
+
+        public bool Electric { get; set; }
+        public override float VehicleSize { get; } = 1.0f;
+    }
+    internal class Bus : Vehicle
+    {
+        public Bus(string licensePlate, string vehicleColor) : base(licensePlate, vehicleColor) { }
+        public int Passengers { get; set; }
+        public override float VehicleSize { get; } = 2;
     }
 }
